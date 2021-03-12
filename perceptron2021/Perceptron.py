8e099d5aca737cd4fc1b2d66df05bb7b46715911
Abdulfattah E. Baalawi <55627035+AbdulfattahBaalawi@users.noreply.github.com>
2021-03-12 21:17:33 +0300

    very simple perceptron algorithm

---
+++ perceptron2021/Perceptron.py
@@ -0,0 +1,44 @@
+"""
+MIT License
+Copyright (c) 2018 Thomas Countz
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import numpy as np
+
+
+class Perceptron(object):
+
+    def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        self.weights = np.zeros(no_of_inputs + 1)
+
+    def predict(self, inputs):
+        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        if summation > 0:
+            activation = 1
+        else:
+            activation = 0
+        return activation
+
+    def train(self, training_inputs, labels):
+        for _ in range(self.threshold):
+            for inputs, label in zip(training_inputs, labels):
+                prediction = self.predict(inputs)
+                self.weights[1:] += self.learning_rate * (label - prediction) * inputs
+                self.weights[0] += self.learning_rate * (label - prediction)
---
+++ perceptron2021/perceptron_OR_binary_binary.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Simple Perceptron Neural Network
+"""
+
+import numpy as np
+
+# training inputs
+
+x = np.array([[1, 1, 1],
+              [1, 0, 1],
+              [0, 1, 1],
+              [0, 0, 1]])
+
+y = np.array([[1],
+              [1],
+              [1],
+              [0]])
+
+
+# sigmoid
+
+def sigmoid(z):
+    return (1 / (1 + np.exp(-z)))
+
+
+
+
+
+
+
+batch = 8
+
+a0 = x
+w =  np.array([0,0,0])
+
+
+
+for cnt in range(batch):
+    batch_x = x
+    batch_y = y
+    n = batch_x.shape[0]
+    # first layer activation value
+    a0 = batch_x
+    count=cnt%4
+
+    # feedforward operation for the first layer
+    z1 = np.dot(a0, w.T)
+
+    t=np.array([batch_y[count],batch_y[count],batch_y[count]]).T
+    net1=np.sum(batch_x[count]*w.T)
+    net2=net1
+    # when theta=0.2
+    if net2>=0.2:
+        net2=1
+    if ((net2<0.2) and (net2>-0.2)):
+        net2=0
+    if net2<=-0.2:
+        net2=-1
+    print(net1)
+    print(net2)
+    print(batch_y[count])
+
+    if net2==batch_y[count][0]:
+        xdelta=[0,0,0]
+    else:
+        xdelta = t[0]*a0[count]
+
+    print("Delta=  {}".format(xdelta))
+    w=np.add(w,xdelta)
+    print ("weights=  {}".format(w))
+    print()
+
+
+
+# test function
+def test_NN(z):
+    k1 = np.dot(z, w)
+
+    return k1
+
+
+# given datas
+x1 = np.array([1, 0, 1])
+x2 = np.array([1, 1, 1])
+print(w)
+
+# output y1 for x1 data
+test_NN(x1)
+print(2*int(round(sigmoid(test_NN(x1))))-1)
+# output y2 for x2 data
+test_NN(x2)
+print(2*(int(round(sigmoid(test_NN(x2)))))-1)
+print("Because the target is binary the network doesnot learn efficiently")
